c7deb4ecb189256fa28e3902c0b1aa6b4fb9e819
life24685 <groshkka@gmail.com>
2020-02-09 18:08:41 +0200

    try cucumber

---
+++ src/test/java/trycucumber/RunTest.java
@@ -0,0 +1,12 @@
+package trycucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features={"src/test/resources"}
+)
+public class RunTest {
+}
